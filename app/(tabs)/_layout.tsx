82f70cefdae04013e088fa4c0f97e80db8f91e39
chdean-09 <chaddenard.andrada-19@cpu.edu.ph>
2023-07-14 22:40:29 +0800

    feature: Add registration page.
    This allows the user to enter their name, email, and password.
    
    Should be able to register the user.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -50,6 +50,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="registration"
+        options={{
+          title: 'Registration',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
