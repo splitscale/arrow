9b4a379ad2cddfca867aa134c4f83a9d25790ed7
Angel Tardaguela <105347598+whatever413y@users.noreply.github.com>
2023-07-24 11:31:55 +0800

    listings

---
+++ service/core-services/location/Listings.ts
@@ -0,0 +1,8 @@
+import { Location } from "./LocationTypes";
+
+export const Listings: Location[] = 
+[
+    { id: "key1", coordinates: { lat: 38, long: 144 }},
+    { id: "key2", coordinates: { lat: 38, long: 141} },
+    { id: "key13", coordinates: { lat: 38, long: 142} },
+]
