c76c4059aeb0eee779bdd5dbd7aa866aae7d0a72
Angel Tardaguela <105347598+whatever413y@users.noreply.github.com>
2023-07-28 13:38:09 +0800

    location service test

---
+++ __tests__/locationService.test.ts
@@ -0,0 +1,50 @@
+import { Location } from '../service/modules/location/type/Location'
+import { LocationService } from "../service/modules/location/LocationService"
+import { LocationWithDistance } from '../service/modules/location/type/LocationWithDistance'
+
+const Locations: Location[] = 
+[
+    { id: "key1", coordinates: { lat: 38, long: 144 }},
+    { id: "key2", coordinates: { lat: 38, long: 141}},
+    { id: "key3", coordinates: { lat: 38, long: 142}},
+]
+const service = new LocationService(Locations)
+
+describe('LocationService searchListings', () => {
+
+    it('Should be able to return a listing', () => {
+        const input = "key1"
+        const expected: Location[] = [ { id: 'key1', coordinates: { lat: 38, long: 144 } }]
+        expect(service.searchListings(input)).toEqual(expected)
+    })
+
+    it('Should return multiple listings', () => {
+        const input = "key"
+        const expected: Location[] = [
+            { id: "key1", coordinates: { lat: 38, long: 144 }},
+            { id: "key2", coordinates: { lat: 38, long: 141 }},
+            { id: "key3", coordinates: { lat: 38, long: 142 }},
+        ]
+        expect(service.searchListings(input)).toEqual(expected)
+    })
+
+    it('Should return an empty array if there is no listing found', () => {
+        const input = "keys"
+        const expected: Location[] = []
+        expect(service.searchListings(input)).toEqual(expected)
+    })
+})
+
+describe('LocationService findLocationsNearby', () => {
+
+    it('Should be able to find nearby locations within 200km', () => {
+        const center = {lat: 38, long: 144}
+        const radius = 200
+    
+        const expected: LocationWithDistance[] = [
+            { id: 'key1', coordinates: { lat: 38, long: 144 }, distance: 0 },
+            { id: 'key3', coordinates: { lat: 38, long: 142 }, distance: 175.24222328428712 }
+        ]
+        expect(service.findLocationsNearby(center, radius)).toEqual(expected)
+    })
+})
