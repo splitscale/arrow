bc4b36c1393d88d03da300e18caa7d1fbc4ffe33
Angel Tardaguela <105347598+whatever413y@users.noreply.github.com>
2023-07-28 12:52:53 +0800

    Delete Locations.ts

--- service/modules/location/Locations.ts
+++
@@ -1,8 +0,0 @@
-import { Location } from "./type/Location";
-
-export const Locations: Location[] = 
-[
-    { id: "key1", coordinates: { lat: 38, long: 144 }},
-    { id: "key2", coordinates: { lat: 38, long: 141}},
-    { id: "key13", coordinates: { lat: 38, long: 142}},
-]
