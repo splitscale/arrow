69064d70d73af3270ab28728370ede96f93ff371
Jerome Cabugwason <jeromecabugwason2@protonmail.com>
2023-08-10 13:54:13 +0800

    fix and enhanced to use bash

--- .ci/update_version.sh
+++ .ci/update_version.sh
@@ -2,82 +2,92 @@
 
 # Function to print usage
 print_usage() {
-    echo "Usage: $0 <major|minor|patch> [-v custom_version] [-p path/to/package.json]"
+    echo "Usage: $0 <major|minor|patch|version_number> [--force] [-p path/to/package.json]"
 }
 
-# Check if argument is provided
-if [ $# -lt 1 ]; then
+# Function to validate version format
+validate_version_format() {
+    local version="$1"
+    if ! echo "$version" | grep -qE '^[0-9]+\.[0-9]+\.[0-9]+$'; then
+        echo "Error: Invalid version format. Expected format: major.minor.patch"
+        exit 1
+    fi
+}
+
+# Function to update version in package.json
+update_version() {
+    local old_version="$1"
+    local new_version="$2"
+    local package_json="$3"
+    sed -i "s/\"version\": \"$old_version\"/\"version\": \"$new_version\"/" "$package_json"
+    echo "Updated version from $old_version to $new_version"
+}
+
+# Set default values
+package_json_path="package.json"
+force_flag=false
+
+# Parse flags using a loop and case statement
+while [ $# -gt 0 ]; do
+    case $1 in
+        -p)
+            package_json_path="$2"
+            shift 2 # Skip the flag and its argument
+        ;;
+        --force)
+            force_flag=true
+            shift 1 # Skip the flag
+        ;;
+        *)
+            first_arg="$1"
+            shift 1 # Skip the argument
+        ;;
+    esac
+done
+
+# Check if an argument is provided
+if [ -z "$first_arg" ]; then
     echo "Error: Missing argument."
     print_usage
     exit 1
 fi
 
-# Check if argument is valid
-if [ "$1" != "major" ] && [ "$1" != "minor" ] && [ "$1" != "patch" ]; then
-    echo "Error: Invalid argument. Must be 'major', 'minor', or 'patch'."
-    print_usage
-    exit 1
-fi
-
-# Set default values for custom_version and package_json_path
-custom_version=""
-package_json_path="package.json"
-
-# Parse flags using a simple loop and case statement
-shift # Skip the first argument
-while [ $# -gt 0 ]; do
-    case "$1" in
-        -v)
-            custom_version="$2"
-            shift 2 # Skip the flag and its argument
-        ;;
-        -p)
-            package_json_path="$2"
-            shift 2 # Skip the flag and its argument
-        ;;
-        *)
-            echo "Error: Invalid option $1" >&2
-            print_usage
-            exit 1
-        ;;
-    esac
-done
-
-# Check if package.json exists.
+# Check if package.json exists
 if [ ! -f "$package_json_path" ]; then
     echo "Error: package.json not found at $package_json_path."
     exit 1
 fi
 
-# Extract current version from package.json.
-current_version=$(grep -oE '"version": "([0-9]+\.[0-9]+\.[0-9]+)' "$package_json_path" | cut -d'"' -f4)
-
-# Validate the version using regex (optional).
-if ! echo "$current_version" | grep -qE '^[0-9]+\.[0-9]+\.[0-9]+$'; then
-    echo "Error: Invalid version found in package.json: $current_version"
-    exit 1
-fi
-
-# Increment the version based on the argument or set it to the custom version provided as an argument using the -v flag.
-if [ -n "$custom_version" ]; then # If custom version is provided, use it.
-    new_version=$custom_version
-else # Otherwise, increment the version based on the first argument using custom logic.
-    IFS='.' read -ra version_parts << EOF
-$current_version
-EOF
-    major="${version_parts[0]}"
-    minor="${version_parts[1]}"
-    patch="${version_parts[2]}"
+# Determine the new version based on the provided argument
+if [[ "$first_arg" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    new_version="$first_arg"
+    if [ "$force_flag" = false ]; then
+        validate_version_format "$new_version"
+    fi
+else
+    if [ "$force_flag" = true ]; then
+        echo "Error: The --force flag can only be used with a version number."
+        exit 1
+    fi
     
-    case "$1" in
+    if [[ ! "$first_arg" =~ ^(major|minor|patch)$ ]]; then
+        echo "Error: Invalid argument. Expected values: major, minor, patch or version_number"
+        print_usage
+        exit 1
+    fi
+    
+    current_version=$(grep -oE '"version": "([0-9]+\.[0-9]+\.[0-9]+)' "$package_json_path" | cut -d'"' -f4)
+    
+    major=$(echo "$current_version" | cut -d. -f1)
+    minor=$(echo "$current_version" | cut -d. -f2)
+    patch=$(echo "$current_version" | cut -d. -f3)
+    
+    case "$first_arg" in
         major)
             major=$((major + 1))
-            minor=0
-            patch=0
         ;;
         minor)
             minor=$((minor + 1))
-            patch=0
         ;;
         patch)
             patch=$((patch + 1))
@@ -87,9 +97,6 @@ EOF
     new_version="$major.$minor.$patch"
 fi
 
-# Replace the old version with the new version in package.json.
-sed -i "s/\"version\": \"$current_version\"/\"version\": \"$new_version\"/" "$package_json_path"
-
-echo "Updated version from $current_version to $new_version"
-
-# changes
+# Replace the old version with the new version in package.json
+current_version=$(grep -oE '"version": "([0-9]+\.[0-9]+\.[0-9]+)' "$package_json_path" | cut -d'"' -f4)
+update_version "$current_version" "$new_version" "$package_json_path"
