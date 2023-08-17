e054e0820a240da6bd49bb393f6afb1058a95a31
kasutu <jeromecabugwason2@protonmail.com>
2023-08-17 11:20:20 +0800

    fix: env not reading
    add `dotenv` for react native

--- service/dataAccess/firebase/firebaseConfigurer.ts
+++ service/dataAccess/firebase/firebaseConfigurer.ts
@@ -1,16 +1,24 @@
 import { FirebaseApp, FirebaseOptions, initializeApp } from 'firebase/app';
+import {
+  API_KEY,
+  APP_ID,
+  AUTH_DOMAIN,
+  MESSAGING_SENDER_ID,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+} from '@env';
 
 export default class FirebaseConfigurer {
   private static app: FirebaseApp;
 
   private static getFirebaseConfig(): FirebaseOptions {
     const firebaseConfig = {
-      apiKey: process.env.EXPO_PUBLIC_API_KEY,
-      authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-      projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-      storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
-      messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
-      appId: process.env.EXPO_PUBLIC_APP_ID,
+      apiKey: API_KEY,
+      authDomain: AUTH_DOMAIN,
+      projectId: PROJECT_ID,
+      storageBucket: STORAGE_BUCKET,
+      messagingSenderId: MESSAGING_SENDER_ID,
+      appId: APP_ID,
     };
 
     return firebaseConfig;
